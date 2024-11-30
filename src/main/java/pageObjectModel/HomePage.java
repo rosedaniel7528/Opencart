39b108cfc481d1928ca2f9f3ee92738900e362cd
Rose <rosedaniel.s7528@gmail.com>
2024-11-30 14:45:13 +0530

    v.03

--- src/main/java/pageObjectModel/HomePage.java
+++ src/main/java/pageObjectModel/HomePage.java
@@ -1,12 +1,9 @@
 package pageObjectModel;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import utilities.ReusableMethods;
 
 public class HomePage {
